0e72adafdc73766acf5515cb7b278c2f3bd1a5d3
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2018-07-29 18:04:15 +0300

    add few calls

--- src/main/java/com/akadatsky/Main.java
+++ src/main/java/com/akadatsky/Main.java
@@ -6,17 +6,38 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
     public static void main(String[] args) {
-        testAddUsers();
-        testGetUsers();
+        addUser("Ivan", 20);
+        addUser("Oleg", 30);
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        getUsers();
+
     }
 
-    private static void testAddUsers() {
+    private static void addUser(String name, int age) {
+        Callback<Void> callback = new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.out("User created");
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.out("FAIL");
+            }
+        };
+        RetrofitClient.getApiService().addUser(name, age).enqueue(callback);
     }
 
-    private static void testGetUsers() {
+    private static void getUsers() {
         Callback<UserList> callback = new Callback<UserList>() {
             @Override
             public void onResponse(Call<UserList> call, Response<UserList> response) {
--- src/main/java/com/akadatsky/api/ApiService.java
+++ src/main/java/com/akadatsky/api/ApiService.java
@@ -2,9 +2,18 @@ package com.akadatsky.api;
 
 import com.akadatsky.model.UserList;
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiService {
+
+    @FormUrlEncoded
+    @POST("user")
+    Call<Void> addUser(
+            @Field("name") String name,
+            @Field("age") int age);
 
     @GET("user")
     Call<UserList> getUsers();
